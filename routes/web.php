6a650accc83113d6601e72f7c8b22180745119c4
Philippe Tassin <p@newnectar.com>
2017-09-19 16:35:21 -0700

    Twilio class call fix

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 //     // Only authenticated users may enter...
 // })->middleware('auth');
 
+
 Route::match(array('GET', 'POST'), '/incoming', function()
-{
+{ 
   //$xml = '<Response><Say>Hello - your app just answered the phone. Neat, eh?</Say></Response>';
-  
-  $twiml = new Services_Twilio_Twiml();
+  $twiml = new Twilio\Twiml();
   $twiml->say('Hello - your app just answered the phone. Neat, eh?', array('voice' => 'alice'));
   $response = Response::make($twiml, 200);
   $response->header('Content-Type', 'text/xml');
