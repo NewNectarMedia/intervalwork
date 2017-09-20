1ce3f2d4def81f98972de24d504e8c74f3cbbb29
Philippe Tassin <p@newnectar.com>
2017-09-20 00:05:37 -0700

    iCal CRLF

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -39,25 +39,25 @@ class CalendarController extends Controller
         // the iCal date format. Note the Z on the end indicates a UTC timestamp.
         define('DATE_ICAL', 'Ymd\THis');
          
-        // max line length is 75 chars. New line is \\n
-        $output = "BEGIN:VCALENDAR\n";
-        $output .= "METHOD:PUBLISH\n";
-        $output .= "VERSION:2.0\n";
-        $output .= "PRODID:-//Interval Work//Topics//EN\n";
+        // max line length is 75 chars. New line is \\r\n
+        $output = "BEGIN:VCALENDAR\r\n";
+        $output .= "METHOD:PUBLISH\r\n";
+        $output .= "VERSION:2.0\r\n";
+        $output .= "PRODID:-//Interval Work//Topics//EN\r\n";
          
         // loop over events
         foreach ($events as $event):
-            $output .= "BEGIN:VEVENT\n";
-            $output .= "SUMMARY:".$event->topic->name."\n";
-            $output .= "UID:intervalwork".$user->slug.$event->id."\n";
-            $output .= "STATUS:CONFIRMED\n";
-            $output .= "DTSTAMP:" . date(DATE_ICAL, strtotime($event->when)) . "\n";
-            $output .= "DTSTART:" . date(DATE_ICAL, strtotime($event->when)) . "\n";
-            $output .= "DTEND:" . date(DATE_ICAL, strtotime($event->when)) . "\n";
-            //$output .= "DATE:" . date('Ymd', strtotime($event->when)) . "\n";
-            $output .= "LAST-MODIFIED:" . date(DATE_ICAL, strtotime($event->when)) . "\n";
-            //$output .= "LOCATION:N/A\n";
-            $output .= "END:VEVENT\n";
+            $output .= "BEGIN:VEVENT\r\n";
+            $output .= "SUMMARY:".$event->topic->name."\r\n";
+            $output .= "UID:intervalwork".$user->slug.$event->id."\r\n";
+            $output .= "STATUS:CONFIRMED\r\n";
+            $output .= "DTSTAMP:" . date(DATE_ICAL, strtotime($event->when)) . "\r\n";
+            $output .= "DTSTART:" . date(DATE_ICAL, strtotime($event->when)) . "\r\n";
+            $output .= "DTEND:" . date(DATE_ICAL, strtotime($event->when)) . "\r\n";
+            //$output .= "DATE:" . date('Ymd', strtotime($event->when)) . "\r\n";
+            $output .= "LAST-MODIFIED:" . date(DATE_ICAL, strtotime($event->when)) . "\r\n";
+            //$output .= "LOCATION:N/A\r\n";
+            $output .= "END:VEVENT\r\n";
         endforeach;
          
         // close calendar
