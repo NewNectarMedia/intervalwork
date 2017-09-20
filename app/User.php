7e3490cef78f03ef60beccdfbd501990c53ff83a
Philippe Tassin <p@newnectar.com>
2017-09-20 16:59:28 -0700

    Mail

--- app/User.php
+++ app/User.php
@@ -3,6 +3,13 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -46,4 +53,5 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Repetition', 'user_id', 'id')->orderBy('when', 'ASC');
     }
+   
 }
